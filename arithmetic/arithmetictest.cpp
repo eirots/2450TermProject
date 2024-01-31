8e47a993d06c29a1548aabaf79790223ef361db0
astorie <adstorie07@gmail.com>
2024-01-31 16:48:56 -0700

    adding base divide functionality

---
+++ arithmetic/arithmetictest.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+#include "Divide.h"
+
+// test cases go here
+void runTests() {
+    // division test cases
+    Divide div;
+    int divResult;
+    int divX = 10, divY = 5;
+    divResult = div.divide(divX, divY);
+    std::cout << "Division test 1: " << divResult << std::endl;
+    divY = 0;
+    divResult = div.divide(divX, divY);
+    std::cout << "Division test 2: " << divResult << std::endl;
+}
+
+int main() {
+    runTests();
+}
