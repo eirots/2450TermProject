5406e3ab17500c2c486a8f8d29cf7f6aa5a0dadd
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-11 21:10:43 -0700

    Updated add.cpp

--- src/arithmetic/add.cpp
+++ src/arithmetic/add.cpp
@@ -2,8 +2,6 @@
 
 Add::Add() {}
 
-int Add::add(int &operand, int operand1)
-{
-    operand += operand1;
-    return operand;
+void Add::execute(UVSimulator& simulator, int operand) {
+    simulator.accumulator += simulator.memory[operand];
 }
