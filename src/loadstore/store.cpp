19d01e9ad19fe68a81bc075845486e885389d18a
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 00:57:24 -0700

    Updated store.cpp with documentation.

--- src/loadstore/store.cpp
+++ src/loadstore/store.cpp
@@ -1,6 +1,15 @@
 #include "store.h"
 #include "../uvsimulator.h"
 
+/*
+ * Executes the store operation.
+
+ * This method stores a value from the accumulator of the UVSimulator into a specific memory location.
+ * param: simulator A reference to the UVSimulator instance on which to perform the store operation.
+ * param: operand The memory location where the value will be stored.
+ * pre: The operand must be a valid index within the bounds of the UVSimulator's memory.
+ * post: The value in the accumulator of the UVSimulator is stored at the specified memory location.
+ */
 void Store::execute(UVSimulator& simulator, int operand) {
     simulator.setMemory(operand, simulator.getAccumulator()); // param1 = location, param2 value
 }
