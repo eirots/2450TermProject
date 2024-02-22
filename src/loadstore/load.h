9e6f7a511943690b19ec1ade511e7698c044a709
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 00:51:09 -0700

    Updated load.h with documentation

--- src/loadstore/load.h
+++ src/loadstore/load.h
@@ -3,6 +3,13 @@
 
 class UVSimulator;
 
+
+/*
+ * class Load
+ * A class to handle the load operation in the program.
+ *
+ * This class provides the functionality to load a value from a specific memory location into the accumulator of the program.
+ */
 class Load {
    public:
     static void execute(UVSimulator& simulator, int operand);
