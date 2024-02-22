1d13c1db199c5268dbeb48ff07c78122c5582e18
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:20:03 -0700

    Updated multiply.h with documentation

--- src/arithmetic/multiply.h
+++ src/arithmetic/multiply.h
@@ -3,6 +3,14 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #ifndef MULTIPLY_H
 #define MULTIPLY_H
 
+
+/*
+ * class Multiply
+ * A class to handle the multiply operation in the UVSimulator.
+ * This class provides the functionality to multiply a value from a specific memory location 
+   with the value in the accumulator.
+ */
+
 class Multiply {
    public:
     Multiply();
