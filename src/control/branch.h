fb1f9db6a67f99b992923e78f948e9b12a1c9760
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:56:14 -0700

    Updated branch.h with documentation.

--- src/control/branch.h
+++ src/control/branch.h
@@ -4,6 +4,14 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #define branch_h
 #include "../uvsimulator.h"
 
+/*
+ * class Branch
+ * A class to handle the branch operation in the UVSimulator.
+ *
+ * This class provides the functionality to change the program counter of the 
+   UVSimulator to a specific memory location, effectively causing the program execution to "branch" to that location.
+ */
+
 class Branch {
    public:
     void BRANCH();
