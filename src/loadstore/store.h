f1f4161244dd843a2c1ca7d3c5ec53b441086448
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 00:59:55 -0700

    Updated store.h with documentation.

--- src/loadstore/store.h
+++ src/loadstore/store.h
@@ -3,6 +3,14 @@
 
 class UVSimulator;
 
+
+/**
+ * class Store
+ * A class to handle the store operation in the UVSimulator.
+ *
+ * This class provides the functionality to store a value from the accumulator of the UVSimulator into a specific memory location.
+ */
+
 class Store {
    public:
     static void execute(UVSimulator& simulator, int operand);
