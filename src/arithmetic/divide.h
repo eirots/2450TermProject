65573510e3619d636d7fdfa4f4f35a7e513551c6
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:13:16 -0700

    Updated divide.h with documentation

--- src/arithmetic/divide.h
+++ src/arithmetic/divide.h
@@ -3,6 +3,14 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #ifndef DIVIDE_H
 #define DIVIDE_H
 
+
+/*
+ * class Divide
+ * A class to handle the divide operation in the UVSimulator.
+ *
+ * This class provides the functionality to divide the value in the accumulator of the UVSimulator 
+   by a value from a specific memory location.
+ */
 class Divide {
    public:
     Divide();
