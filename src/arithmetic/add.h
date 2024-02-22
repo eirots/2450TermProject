16848758c2b5e4035cb098d0662737129c111547
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:05:32 -0700

    Updated add.h with documentation

--- src/arithmetic/add.h
+++ src/arithmetic/add.h
@@ -4,6 +4,14 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #ifndef ADD_H
 #define ADD_H
 
+
+/*
+ * class Add
+ * A class to handle the add operation in the UVSimulator.
+ *
+ * This class provides the functionality to add a value from a specific memory location to the accumulator of the UVSimulator.
+ */
+
 class Add {
    public:
     Add();
