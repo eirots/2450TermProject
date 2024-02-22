46e801220158fb95e27ad8c6c56e8679606249e1
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 00:37:57 -0700

    Updated with documentation.

--- src/uvsimulator.cpp
+++ src/uvsimulator.cpp
@@ -6,17 +6,32 @@
 
 using namespace std;
 
-// Constructor
+/*
+ Constructor for the UVSimulator class.
+ Initializes the memory to 100 locations with 0, accumulator to 0, and program counter to 0.
+ */
 UVSimulator::UVSimulator() : memory(100, 0), accumulator(0), pc(0) {}
 
-// Load input program into memory
+/*
+ * Loads a program into the memory.
+ 
+ * param: program A vector of integers representing the program to be loaded.
+ * pre: The program must be a valid sequence of instructions.
+ * post: The program is loaded into the memory.
+ */
 void UVSimulator::loadProgram(const vector<int>& program) {
     for (int i = 0; i < program.size(); ++i) {
         memory[i] = program[i];
     }
 }
 
-// Get a value from memory location
+/*
+ * Get a value from memory location.
+ 
+ * param: The index of the memory location.
+ * pre: The index must be within the bounds of the memory size.
+ * post: Returns the value at the specified index in the memory. If the index is out of bounds, returns 0.
+ */
 int UVSimulator::getMemory(int index) const {
     if (index >= 0 && index < memory.size()) {
         return memory[index];
@@ -25,7 +40,13 @@ int UVSimulator::getMemory(int index) const {
     }
 }
 
-// Set a value to memory location
+/* 
+ * Set a value to memory location
+
+ * param index The index of the memory location, and the value to be set at the specified memory location.
+ * pre: The index must be within the bounds of the memory size.
+ * post: The value at the specified index in the memory is set to the provided value. If the index is out of bounds, does nothing.
+ */
 void UVSimulator::setMemory(int index, int value) {
     if (index >= 0 && index < memory.size()) {
         memory[index] = value;
@@ -33,32 +54,63 @@ void UVSimulator::setMemory(int index, int value) {
     }  // does nothing if index is out of bounds
 }
 
-// gets size of memory array
+/*
+ * gets size of memory array
+
+ * pre: None.
+ * post: Returns the size of the memory array.
+ */
 int UVSimulator::getMemSize() {
     return memory.size();
 }
 
-// Get value from accumulator
+/*
+ * Get value from accumulator
+ * pre: None.
+ * post: Returns the current value of the accumulator.
+ */
 int UVSimulator::getAccumulator() const {
     return accumulator;
 }
 
-// Set value to accumulator
+/* 
+ * Set value to accumulator
+
+ * param: value The value to be set in the accumulator.
+ * pre: None.
+ * post: The accumulator is set to the provided value.
+ */
 void UVSimulator::setAccumulator(int value) {
     accumulator = value;
 }
 
-// sets value of PC for jump operations
+/*
+ * sets value of PC for jump operations
+
+ * param: value The value to be set in the program counter.
+ * pre: None.
+ * post: The program counter is set to the provided value.
+ */
 void UVSimulator::setPC(int value) {
     pc = value;
 }
 
-// gets value of PC
+/* 
+ * gets value of PC
+
+ * pre None.
+ * post Returns the current value of the program counter.
+ */
 int UVSimulator::getPC() {
     return pc;
 }
 
-// Build a program
+/*
+ * Build a program
+ 
+ * pre: None.
+ * post: Returns a vector of integers representing the program built by the user.
+ */
 vector<int> UVSimulator::buildProgram() {
     vector<int> program;
     string userInput;
@@ -83,6 +135,12 @@ vector<int> UVSimulator::buildProgram() {
     return program;
 }
 
+/*
+ * Prints the contents of the memory.
+ *
+ * pre: None.
+ * post: The contents of the memory are printed to the console.
+ */
 void UVSimulator::printMemory() const {
     cout << "Memory contents:" << endl;
     for (int i = 0; i < memory.size(); ++i) {
@@ -90,7 +148,12 @@ void UVSimulator::printMemory() const {
     }
 }
 
-// Execute the program
+/*
+ * Execute the program
+ 
+ * pre: A program must be loaded into the memory.
+ * post: The program is executed and the state of the memory, accumulator, and program counter may be changed.
+ */
 void UVSimulator::executeProgram() {
     while (true) {
         int instruction = memory[pc];     // Reads instruction from memory (starts at 00)
