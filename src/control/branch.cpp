386b401f76df78b1c3cd072e46c0860720eae2b6
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:48:10 -0700

    Updated branch.cpp with documentation

--- src/control/branch.cpp
+++ src/control/branch.cpp
@@ -3,6 +3,16 @@
 void Branch::BRANCH() {
 }
 
+
+/*
+ * Executes the branch operation.
+ * This method changes the program counter of the UVSimulator to a specific memory location.
+ 
+ * param: simulator A reference to the UVSimulator instance on which to perform the branch operation, and operand, the 
+   memory location to which the program counter will be set.
+ * pre: The operand must be a valid index within the bounds of the memory.
+ * post: The program counter is set to the specified memory location.
+ */
 void Branch::execute(UVSimulator &simulator, int operand) {
     simulator.setPC(operand);
 }
