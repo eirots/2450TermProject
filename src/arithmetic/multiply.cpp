db54cdff7aa4f531ad8cf52a9a65b3577878636e
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:17:23 -0700

    Updated multiply.cpp with documentation

--- src/arithmetic/multiply.cpp
+++ src/arithmetic/multiply.cpp
@@ -3,6 +3,16 @@
 #include "../uvsimulator.h"
 Multiply::Multiply() {}
 
+/*
+ * Executes the multiply operation.
+ 
+ * This method multiplies a value from a specific memory location with the value in the accumulator.
+ * param: sim A reference to the UVSimulator instance on which to perform the multiply operation.
+ * param: operand The memory location from which to get the value to be multiplied.
+ * pre: The operand must be a valid index within the bounds of the memory.
+ * post: The value in the accumulator  is multiplied by the value at the specified memory location.
+ */
+void 
 void Multiply::execute(UVSimulator& sim, int operand) {
     int product = sim.getAccumulator();
     product *= sim.getMemory(operand);
