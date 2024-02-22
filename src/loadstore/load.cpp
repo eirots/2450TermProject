80f9218a160c853db751ade18ec0f1f573fcc053
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 00:50:34 -0700

    Updated load.cpp with documentation

--- src/loadstore/load.cpp
+++ src/loadstore/load.cpp
@@ -1,6 +1,15 @@
 #include "load.h"
 #include "../uvsimulator.h"
 
+
+/*
+ * Executes the load operation.
+ 
+ * This method loads a value from a specific memory location into the accumulator of the UVSimulator.
+ * param: simulator, A reference to the UVSimulator instance where the load operation will be performed,and operand the memory location from which to load the value.
+ * pre: The operand must be a valid index within the bounds of the program's memory.
+ * post: The accumulator of the program is set to the value at the specified memory location.
+ */
 void Load::execute(UVSimulator& simulator, int operand) {
     simulator.setAccumulator(simulator.getMemory(operand));
 }
