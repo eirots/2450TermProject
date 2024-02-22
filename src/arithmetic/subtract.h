4be4eecd9417bea1cb99fb2644ccaa33198f35fd
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:30:51 -0700

    Updated subtract.h with documentation

--- src/arithmetic/subtract.h
+++ src/arithmetic/subtract.h
@@ -3,6 +3,14 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #ifndef SUBTRACT_H
 #define SUBTRACT_H
 
+
+/*
+ * class Subtract
+ * A class to handle the subtract operation in the UVSimulator.
+ * This class provides the functionality to subtract a value from a specific memory location 
+   from the value in the accumulator.
+ */
+
 class Subtract {
    public:
     Subtract();
