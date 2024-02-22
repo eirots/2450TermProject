d083c8a07c90b8af3d2769afad0d0918ff844f50
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:12:07 -0700

    Updated divide.cpp with documentation

--- src/arithmetic/divide.cpp
+++ src/arithmetic/divide.cpp
@@ -3,7 +3,19 @@
 #include "../uvsimulator.h"
 // but the sign of a data word may be either plus or minus
 Divide::Divide() {}
-// Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
+/*
+ * Executes the divide operation.
+
+ * This method divides the value in the accumulator by a value from a specific memory location. 
+   If the divisor is zero, it throws an exception.
+ * param: sim, A reference to the UVSimulator instance on which to perform the divide operation, and operand 
+   the memory location from which to get the divisor.
+ * param: operand The memory location from which to get the divisor.
+ * pre: The operand must be a valid index within the bounds of the UVSimulator's memory. The value at the memory location 
+   must not be zero.
+ * post: The value in the accumulator is divided by the value at the specified memory location. 
+   If the divisor is zero, an exception is thrown.
+ */
 void Divide::execute(UVSimulator& sim, int operand) {
     int result;
     int divisor = sim.getMemory(operand);
