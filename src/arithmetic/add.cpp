3e6e2a578df443e1bea363fc2802263e5c99fd02
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-22 01:03:31 -0700

    Updated add.cpp with documentation

--- src/arithmetic/add.cpp
+++ src/arithmetic/add.cpp
@@ -4,6 +4,16 @@
 
 Add::Add() {}
 
+
+/*
+ * Executes the add operation.
+ * This method adds a value from a specific memory location to the accumulator of the UVSimulator.
+
+ * param: simulator, a reference to the UVSimulator instance on which to perform the add operation.
+ * param: operand, the memory location from which to get the value to be added.
+ * pre: The operand must be a valid index within the bounds of the UVSimulator's memory.
+ * post: The value in the accumulator of the UVSimulator is increased by the value at the specified memory location.
+ */
 void Add::execute(UVSimulator& simulator, int operand) {
     int holder = simulator.getAccumulator();
     holder += simulator.getMemory(operand);
