6ccaea16534a7fd08408d7dd261f56ad76b8c0e3
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-24 14:30:36 -0700

    Updated read.cpp with documentation

--- src/io/read.cpp
+++ src/io/read.cpp
@@ -6,12 +6,12 @@
 
 /*
  * Executes the read operation.
- * This method reads an integer from the user and stores it in a specific memory location in the UVSimulator. 
+ * This method reads an integer from the user and stores it in a specific memory location. 
    If the memory location is out of range, it throws an exception.
  * param: simulator, A reference to the UVSimulator instance on which to perform the read operation, and operand, 
    The memory location where the user's input will be stored.
- * pre: The operand must be a valid index within the bounds of the UVSimulator's memory.
- * post: The user's input is read and stored at the specified memory location in the UVSimulator. If the memory location 
+ * pre: The operand must be a valid index within the bounds of the memory.
+ * post: The user's input is read and stored at the specified memory location. If the memory location 
    is out of range, an exception is thrown.
  */
 void Read::execute(UVSimulator& simulator, int operand) {
