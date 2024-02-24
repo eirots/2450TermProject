a9f5c5cc6a06b7cc894fb0b2a502e3fcd8050baa
jmarin255 <143770508+jmarin255@users.noreply.github.com>
2024-02-24 14:14:19 -0700

    Updated read.h with documentation

--- src/io/read.h
+++ src/io/read.h
@@ -3,6 +3,13 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 #ifndef READ_H
 #define READ_H
 
+
+/*
+ * class Read
+ * A class to handle the read operation in the UVSimulator.
+ * This class provides the functionality to read an integer from the user and store it in a specific memory location in the UVSimulator.
+ */
+
 class Read {
    public:
     void execute(UVSimulator& simulator, int operand);
