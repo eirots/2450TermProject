ade8f6fac18ecfaef3dbdcca43c5fa5ce5ee3920
astorie <adstorie07@gmail.com>
2024-02-26 19:29:55 -0700

    first bit of refactor, research on how to do this is done so the rest should be fast

--- src/arithmetic/multiply.h
+++ src/arithmetic/multiply.h
@@ -1,20 +1,19 @@
 class UVSimulator;  // forward declaration to avoid cyclical declarations
-
+#include "arithmetic.h"
 #ifndef MULTIPLY_H
 #define MULTIPLY_H
-
 
 /*
  * class Multiply
  * A class to handle the multiply operation in the UVSimulator.
- * This class provides the functionality to multiply a value from a specific memory location 
+ * This class provides the functionality to multiply a value from a specific memory location
    with the value in the accumulator.
  */
 
-class Multiply {
+class Multiply : public Arithmetic {
    public:
     Multiply();
 
-    void execute(UVSimulator& sim, int operand);
+    void execute(UVSimulator& sim, int operand) override;
 };
 #endif  // MULTIPLY_H
