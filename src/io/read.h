3d98a411940b6e659f84d64d68dcd29e7032f972
astorie <adstorie07@gmail.com>
2024-02-26 19:42:01 -0700

    io refactor

--- src/io/read.h
+++ src/io/read.h
@@ -2,10 +2,11 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 
 #ifndef READ_H
 #define READ_H
+#include "io.h"
 
-class Read {
+class Read : public IO {
    public:
-    void execute(UVSimulator& simulator, int operand);
+    void execute(UVSimulator& simulator, int operand) override;
 };
 
 #endif  // READ_H
--- src/io/write.h
+++ src/io/write.h
@@ -2,14 +2,15 @@ class UVSimulator;  // forward declaration to avoid cyclical declarations
 
 #ifndef WRITE_H
 #define WRITE_H
+#include "io.h"
 
 const int MEMORY_SIZE = 100;  // Define the size of the memory array
 
 extern int memory[MEMORY_SIZE];
 
-class Write {
+class Write : public IO {
    public:
-    void execute(UVSimulator& simulator, int operand);
+    void execute(UVSimulator& simulator, int operand) override;
 };
 
 #endif
