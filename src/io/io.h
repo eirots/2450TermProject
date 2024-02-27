f1bba1004926251d8e08aaa60044a9923d363ca9
astorie <adstorie07@gmail.com>
2024-02-26 19:43:12 -0700

    whoops forgot a file

--- src/io/io.h
+++ src/io/io.h
@@ -3,10 +3,10 @@ class UVSimulator;  // forward declaration to avoid circular dependancies
 #ifndef IO_H
 #define IO_H
 
-/*Purely virtual class for ensuring that other Arithmetic classes derive from this. */
+/*Purely virtual class for ensuring that other i/o classes derive from this. */
 class IO {
    public:
     virtual void execute(UVSimulator& sim, int operand) = 0;  // ensuring that there is no base functionalty, purely virtual function
 };
 
-#endif  // IO_h
+#endif  // IO_H
