cf17cd68c95d8d8572f119d95dfeb0e9d478455d
astorie <adstorie07@gmail.com>
2024-02-26 19:38:35 -0700

    fixed something in control.h virtual class

--- src/control/control.h
+++ src/control/control.h
@@ -3,7 +3,7 @@ class UVSimulator;  // forward declaration to avoid circular dependancies
 #ifndef CONTROL_H
 #define CONTROL_H
 
-/*Purely virtual class for ensuring that other Arithmetic classes derive from this. */
+/*Purely virtual class for ensuring that other Control classes derive from this. */
 class Control {
    public:
     virtual void execute(UVSimulator& sim, int operand) = 0;  // ensuring that there is no base functionalty, purely virtual function
